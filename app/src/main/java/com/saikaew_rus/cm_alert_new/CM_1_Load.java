05388e14ed84be5001af860dab50b2e7767a1196
kanwchai <kanwchai@hotmail.com>
2016-12-13 12:02:49 +0700

    13-12-16

--- app/src/main/java/com/saikaew_rus/cm_alert_new/CM_1_Load.java
+++ app/src/main/java/com/saikaew_rus/cm_alert_new/CM_1_Load.java
@@ -36,7 +36,7 @@ public class CM_1_Load extends AppCompatActivity {
                 try {
                     int waited = 0;
                     // Splash screen pause time
-                    while (waited < 2500) {
+                    while (waited < 1500) {
                         sleep(100);
                         waited += 100;
                     }
